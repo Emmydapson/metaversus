776d8702f16c352aa4e01cbb5175e3bb3a6b4cf6
emmydapson <iloriemmanuel00@gmail.com>
2023-12-05 20:28:10 +0100

    feedback, world, insight and footer sections all creaated

--- sections/World.jsx
+++ sections/World.jsx
@@ -27,8 +27,21 @@ const World = () => (
         className="relative mt-[68px] flex w-full h-[550px]"
       >
         <img src="/map.png" alt="map" className="w-full h-full object-cover" />
-        <div className="absolute bottom-20 right-20 w [70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]">
-          <img src="people-01.png" alt="" />
+        <div className="absolute bottom-20 right-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]">
+          <img src="people-01.png" alt="people" className="w-full h-full" />
+        </div>
+        <div className="absolute top-10 left-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]">
+          <img src="people-02.png" alt="people" className="w-full h-full" />
+        </div>
+        <div className="absolute top-1/2 left-[45%] w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]">
+          <img src="people-03.png" alt="people" className="w-full h-full" />
+        </div>
+        <div className="absolute top-1/2 left-5 w-[196px] h-[150px] p-[6px] square-full bg-[#5d6680] rounded-3xl">
+          <img src="Mask Group-left.png" alt="people" className="w-full h-full" />
+          <img src="ellipse 44" alt="person" className="w-6 h-6 rounded-full" />
+        </div>
+        <div className="absolute top-1/4 right-[35%] w-[196px] h-[150px] p-[6px] square-full bg-[#5d6680] rounded-3xl">
+          <img src="Mask Group.png" alt="people" className="w-full h-full" />
         </div>
       </motion.div>
     </motion.div>
